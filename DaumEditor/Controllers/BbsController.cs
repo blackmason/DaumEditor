cc445537ef00eb9030ad2a2aff0e70bb2914860b
Mason Briekroak <blackmason805@gmail.com>
2016-09-20 00:14:49 +0900

    Daum Editor TEST - Write/Image Upload

---
+++ DaumEditor/Controllers/BbsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace DaumEditor.Controllers
+{
+    public class BbsController : Controller
+    {
+        //
+        // GET: /Bbs/
+
+        public ActionResult Write()
+        {
+            return View();
+        }
+
+        public ActionResult ImageUpload()
+        {
+            return View();
+        }
+
+        public JsonResult SubmitAttach(HttpPostedFileBase images)
+        {
+            string fileName = null;
+            string filePath = null;
+
+            if (images.ContentLength > 0)
+            {
+                fileName = Path.GetFileName(images.FileName);
+                filePath = Path.Combine(Server.MapPath("../Uploads"), fileName);
+                images.SaveAs(filePath);
+            }
+
+            return Json(filePath, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
